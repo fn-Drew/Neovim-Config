7506541f3ae36ff0ce30997b95828fc704d211ef
fn-Drew <alutter03@gmail.com>
2023-02-04 12:20:16 -0500

    initial commit with treesitter, harpoon, undotree, and vim-fugitive

---
+++ lua/bean/packer.lua
@@ -0,0 +1,34 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+ -- use { "catppuccin/nvim", as = "catppuccin" }
+  -- vim.cmd.colorscheme 'catppuccin-mocha'
+
+  use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		  require("rose-pine").setup()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
+
+ -- Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+end)
---
+++ lua/bean/remap.lua
@@ -0,0 +1,3 @@
+vim.g.mapleader = " "
+-- (mode, keybind, command that it executes)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
