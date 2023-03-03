465b6024217e88f1bdc595461fbf7861b7e19d6e
fn-Drew <alutter03@gmail.com>
2023-03-03 17:54:55 -0500

    code completion with lsp

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,13 +1,18 @@
 local null_ls = require("null-ls")
 
-local formatting = null_ls.builtins.formatting
+local code_actions = null_ls.builtins.code_actions
 local diagnostics = null_ls.builtins.diagnostics
+local formatting = null_ls.builtins.formatting
+local hover = null_ls.builtins.hover
+local completion = null_ls.builtins.completion
 
+-- diagnostics and formatting
 null_ls.setup({
-    sources = {
-        -- formatting.prettier,
-        -- formatting.prettier,
-        diagnostics.eslint_d,
-        formatting.eslint_d,
-    }
+	sources = {
+		-- lua
+		formatting.stylua,
+		-- web dev
+		diagnostics.eslint_d,
+		formatting.eslint_d,
+	},
 })
